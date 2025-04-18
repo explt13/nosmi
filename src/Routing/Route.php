fab27dccb7781d876616b3ad6715caf485e56012
explt13 <az13rede4d@gmail.com>
2025-04-19 01:26:51 +0500

    Refactor Route, extends API

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -7,182 +7,171 @@ use Explt13\Nosmi\Exceptions\InvalidAssocArrayValueException;
 
 class Route
 {
-    private array $route = [];
+    private array $params = [];
+    private string $controller;
+    private string $regexp;
     private static array $routes = [];
-    private array $readOnlyProperties = [];
-    private bool $canOverwrite = false;
+    private static array $patterns_map = [];
     private const PATH_PARAMETERS_TYPES = ['<string>' => '[a-zA-Z]+', '<int>' => '[0-9]+', '<slug>' => '[a-zA-Z0-9-]+'];
 
     public function setRoute(string $path): void
     {
-        $this->parseRoute($path);
-        // $route = $this->prepareRoute($route);
-        // foreach ($route as $key => $value) {
-        //     $this->route[$key] = $value;
-        //     $this->readOnlyProperties[] = $key;
-        // }
+        if (empty(self::$routes)) {
+            throw new \LogicException('No routes found, make sure you added them correctly.');
+        }
+        $this->resolvePath($path);
     }
 
-    public static function add(string $path, string $controller): void
+    public function getController(): string
     {
-        $regexp = self::convertPathToPathPattern($path);
+        return $this->controller;
+    }
+
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+    
+    public function getPathRegexp(): string
+    {
+        return $this->regexp;
+    }
+
+    public function getPathPattern(): string
+    {
+        return array_search($this->regexp, self::$patterns_map);
+    }
+
+    public static function add(string $path_pattern, string $controller): void
+    {
+        $regexp = self::convertPathPatternToRegexp($path_pattern);
+        self::$patterns_map[$path_pattern] = $regexp;
         self::$routes[$regexp] = $controller;
     }
 
-    private static function convertPathToPathPattern(string $path): string
+    public static function getPatternToRegexMap(): array
     {
-        $needs_convert = substr_count($path, ':');
-
-        // If there is nothing to convert, return path
-        if (empty($needs_convert)) return $path;
-
-        preg_match_all('#(?P<type><[a-z]+>|(?<=/))?:(?P<path_parameter>[a-z_]+)(?=/|$)#', $path, $wildcards, PREG_SET_ORDER | PREG_UNMATCHED_AS_NULL);
-
-        // If numbers of needed conversion are not equal to matched wildcards;
-        if ($needs_convert !== count($wildcards)) {
-            $actual_count = count($wildcards);
-            throw new \LogicException("Path: $path has the count of matched path parameters ($actual_count) less than needs to be converted ($needs_convert). Check path's named parameters syntax.");
-        }
-       
-        
-        $wildcards = array_map(function($wildcard) use ($path) {
-            $filtered = array_filter($wildcard, fn($key) => !is_int($key), ARRAY_FILTER_USE_KEY);
-
-            if (is_null($filtered['type'])) {
-                throw InvalidAssocArrayValueException::withMessage("Path: $path cannot be converted, `type` key is null for parameter: {$filtered['path_parameter']}. Check path's named parameters syntax.");
-            }
-
-            // if /:parameter then automatically set <slug> type
-            if ($filtered['type'] === "") {
-                $filtered['type'] = "<slug>";
-            }
-            return $filtered;
-        }, $wildcards);
-
-        foreach($wildcards as $wildcard) {
-            $type = $wildcard['type'];
-            $param_name = $wildcard['path_parameter'];
-            if (!in_array($type, array_keys(self::PATH_PARAMETERS_TYPES))) {
-                throw new InvalidAssocArrayValueException('type', array_keys(self::PATH_PARAMETERS_TYPES), $type);
-            }
-            switch ($type) {
-                case '<slug>':
-                    $path = preg_replace(
-                        "#($type)?:$param_name#",
-                        sprintf("(?P<%s>%s)", $param_name, self::PATH_PARAMETERS_TYPES[$type]),
-                        $path
-                    );
-                    break;
-                case '<string>':
-                    $path = preg_replace(
-                        "#$type:$param_name#",
-                        sprintf("(?P<%s>%s)", $param_name, self::PATH_PARAMETERS_TYPES[$type]),
-                        $path
-                    );
-                    break;
-                case '<int>':
-                    $path = preg_replace(
-                        "#$type:$param_name#",
-                        sprintf("(?P<%s>%s)", $param_name, self::PATH_PARAMETERS_TYPES[$type]),
-                        $path
-                    );
-                    break;
-            }
-        }
-        return $path;
+        return self::$patterns_map;
     }
 
-    public function getRoutesPathPatterns()
+    public static function getPathPatterns(): array
     {
-        return array_keys(self::$routes);
+        return array_keys(self::$patterns_map);
     }
 
-    public function getPathPatternFromPath($path)
+    public static function getPathRegexps(): array
     {
-        return self::convertPathToPathPattern($path);
+        return array_values(self::$patterns_map);
     }
 
-    public function getControllerByPath($path)
+    public static function getRoutes(): array
     {
-        $regexp = self::convertPathToPathPattern($path);
+        return self::$routes;
+    }
+
+    public static function getRegexpByPathPattern(string $path_pattern): string
+    {
+        return self::$patterns_map[$path_pattern];
+    }
+
+    public static function getControllerByPathPattern(string $path_pattern): string
+    {
+        $regexp = self::$patterns_map[$path_pattern];
         return self::$routes[$regexp];
     }
 
-    public function getControllerByPathPattern($path_pattern)
+    public static function getControllerByRegexp(string $regexp): string
     {
-        return self::$routes[$path_pattern];
+        return self::$routes[$regexp];
     }
 
-    private function parseRoute($path)
+    public static function getControllerPatternPaths(string $controller): array
+    {
+        $regexps = self::getControllerRegexps($controller);
+        $patterns = array_keys(array_filter(self::$patterns_map, function($regexp) use ($regexps) {
+            return in_array($regexp, $regexps, true);
+        }));
+        return $patterns;
+    }
+
+    public static function getControllerRegexps(string $controller): array
+    {
+        return array_keys(self::$routes, $controller, true);
+    }
+
+    private static function convertPathPatternToRegexp(string $path_pattern): string
+    {
+        $needs_convert = substr_count($path_pattern, ':');
+
+        // If there is nothing to convert, return path
+        if (empty($needs_convert)) return $path_pattern;
+
+        $path_pattern = self::normalizePathPattern($path_pattern);
+
+        preg_match_all('#(?P<type><[a-z]+>):(?P<name>[a-z_]+)(?=/|$)#', $path_pattern, $path_parameters, PREG_SET_ORDER | PREG_UNMATCHED_AS_NULL);
+
+        // If numbers of needed conversion are not equal to matched path_parameters;
+        if ($needs_convert !== count($path_parameters)) {
+            $actual_count = count($path_parameters);
+            throw new \LogicException("Path: $path_pattern has the count of matched path parameters ($actual_count) less than needs to be converted ($needs_convert). Check path's named parameters syntax.");
+        }
+        $path_parameters = array_map(fn($wildcard) => array_filter($wildcard, fn($key) => !is_int($key), ARRAY_FILTER_USE_KEY), $path_parameters);
+
+        foreach($path_parameters as $path_parameter) {
+            $type = $path_parameter['type'];
+            $name = $path_parameter['name'];
+            self::validatePathParameterType($type);
+            $path_pattern = self::makeRegexConversion($type, $name, $path_pattern);
+        }
+        return $path_pattern;
+    }
+
+    private static function normalizePathPattern(string $path_pattern): string
+    {
+        return preg_replace_callback(
+            '#(?<=/):([a-z_]+)#',
+            function ($matches) {
+                // If type is missing, inject default type
+                return "<slug>:{$matches[1]}";
+            },
+            $path_pattern
+        );
+    }
+
+    private static function makeRegexConversion(string $type, string $name, string $path_pattern): string
+    {
+        return preg_replace(
+            "#$type:$name#",
+            sprintf("(?P<%s>%s)", $name, self::PATH_PARAMETERS_TYPES[$type]),
+            $path_pattern
+        );
+    }
+
+
+    private static function validatePathParameterType(string $type): void
+    {
+        if (!in_array($type, array_keys(self::PATH_PARAMETERS_TYPES))) {
+            throw new InvalidAssocArrayValueException('type', array_keys(self::PATH_PARAMETERS_TYPES), $type);
+        }
+    }
+
+    private function resolvePath(string $path): void
     {
         foreach (self::$routes as $regexp => $controller) {
-            if (preg_match("#$regexp#", $path, $matches)) {
-
+            if (preg_match("#$regexp#", $path, $parameters)) {
+                $this->regexp = $regexp;
+                $this->controller = $controller;
+                $parameters = array_filter($parameters, fn($key) => !is_int($key), ARRAY_FILTER_USE_KEY);
+                $this->setPathParams($parameters);
             }
             throw new \Exception("Route `$path` is not found", 404);
         }
-
     }
 
-    private function prepareRoute(array $route): array
+    private function setPathParams(array $parameters): void
     {
-        if (!isset($route['layout'])) {
-            $route['layout'] = FRAMEWORK . "/Templates/Views/Errors/dev.php";
+        foreach ($parameters as $name => $value) {
+            $this->params[$name] = $value;
         }
-        if (empty($route['action'])) {
-            $route['action'] = 'index';
-        }
-        $route['controller'] = $this->upperCamelCase($route['controller']);
-        return $route;
-    }
-
-    /**
-     * @var string $route
-     * @return array route params [...default_params, ...$concrete_params];
-     * @throws \Exception
-     */
-    private function getRouteParams(string $route): array
-    {
-        foreach ($this->routes as $pattern => $default_params) {
-            if (preg_match("#{$pattern}#", $route, $matches)) {
-                $matches = array_filter($matches, fn($key) => !is_int($key), ARRAY_FILTER_USE_KEY);
-                return [...$default_params, ...$matches];
-            }
-        }
-        throw new \Exception("Route `$route` is not found", 404);
-    }
-
-    private function extractRouteFromQueryString(string $url): string
-    {
-        if ($url) {
-            $route = explode("&", $url, 2)[0];
-            if (strpos($route, "=") === false) {
-                return rtrim($route, '/');
-            }
-        }
-        return '';
-    }
-
-   
-
-    private function upperCamelCase(string $str): string
-    {
-        return str_replace('-', '', ucwords($str, '-'));
-    }
-
-
-    public function __get(string $name): mixed
-    {
-        return $this->route[$name] ?? null;
-    }
-
-    public function __isset(string $name): bool
-    {
-        return isset($this->route[$name]);
-    }
-
-    public function toArray(): array
-    {
-        return $this->route;
     }
 }
