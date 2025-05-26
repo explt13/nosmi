bf5af72a79ceb3925a761b11547be2aa2ba1818d
explt13 <az13rede4d@gmail.com>
2025-05-26 22:31:12 +0500

    Create WidgetFactory, refactor Widget

--- src/Base/Widget.php
+++ src/Base/Widget.php
@@ -7,11 +7,7 @@ use Explt13\Nosmi\Interfaces\WidgetInterface;
 
 abstract class Widget implements WidgetInterface
 {
-    private ?string $tpl = null;
-    public function __construct(string $path_to_tpl)
-    {
-        $this->tpl = $path_to_tpl;
-    }
+    protected ?string $tpl = null;
 
     public function render(): string
     {
